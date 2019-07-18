79bb37048a89a45aa20e2098c1af433d7791a425
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-07-18 18:14:48 -0400

    ProjetoDotNetTask01 - Inicio

---
+++ Instrutor/ExbirFrase01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDotNetTask01.Instrutor
+{
+    class ExbirFrase01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ExbirFrase01 - Hello World!");
+        }
+    }
+}
