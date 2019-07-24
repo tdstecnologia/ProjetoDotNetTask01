848ce33a678df85170ef64150726727bcfc355c3
Ivan <Ivan@SVG.local>
2019-07-24 18:16:35 -0400

    [Task30] - Calcular área do quadrado

---
+++ Ivan/CalcularAreaQuadradoTask30.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDotNetTask01.Ivan
+{
+    class CalcularAreaQuadradoTask30
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("PROGRAMA PARA CALCULAR A ÁREA DE UM QUADRADO, DADA A MEDIDA DE UM DOS LADOS");
+            Console.WriteLine("INFORME A MEDIDA DE UM DOS LADOS");
+            var L = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(L * L);
+        }
+    }
+}
