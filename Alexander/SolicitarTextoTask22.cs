52e71ac1cfe6affc234f5b105d1c19d563fed611
Alexander <debarbara4@gmail.com>
2019-07-23 22:03:11 -0400

    [task-22] - Solicitar Texto

---
+++ Alexander/SolicitarTextoTask22.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDotNetTask01.Alexander
+{
+    class SolicitarTextoTask22
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um Texto");
+            var texto = Console.ReadLine();
+            Console.WriteLine("Seu Texto é: " + Convert.ToString(texto));
+        }
+    }
+}
