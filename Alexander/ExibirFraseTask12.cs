745db7146a6d9411498780967d7867216aecada5
Alexander <debarbara4@gmail.com>
2019-07-23 21:21:10 -0400

    [task-12] - Exibir Frase

---
+++ Alexander/ExibirFraseTask12.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDotNetTask01.Alexander
+{
+    class ExibirFraseTask12
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Thor, sua mãe sabe que você está usando as cortinas dela? (Homem de Ferro)");
+        }
+
+    }
+}
