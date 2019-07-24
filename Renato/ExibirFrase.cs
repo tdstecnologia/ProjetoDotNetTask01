0557b37b91600d98439b2f98563680dc660979d2
Renato Rodrigues <renato_rodrigues2@yahoo.com.br>
2019-07-23 21:22:37 -0400

    [task-17] - Exibir Frase

---
+++ Renato/ExibirFrase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDotNetTask01.Renato
+{
+    class ExibirFrase
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thor, sua mãe sabe que você está usando as cortinas dela?");
+            Console.WriteLine("Homem de Ferro");
+        }
+    }
+}
