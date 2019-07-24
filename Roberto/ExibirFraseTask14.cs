e3730e3e0ec217880f7201afadfb5f35c7f3a2b9
Ivan <ivanzeitoun@gmail.com>
2019-07-23 21:21:22 -0400

    [task-14] - Exibir Frase

---
+++ Roberto/ExibirFraseTask14.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDotNetTask01.Roberto
+{
+    class ExibirFraseTask14
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Thor, sua mãe sabe que você está usando as cortinas dela?");
+        }
+    }
+}
