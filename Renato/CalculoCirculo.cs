83b44cb6be9567ef4d487f11346a7577699c47d5
Renato Rodrigues <renato_rodrigues2@yahoo.com.br>
2019-07-25 22:13:34 -0400

    Task-9999

---
+++ Renato/CalculoCirculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDotNetTask01.Renato
+{
+    class CalculoCirculo
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            double A, Raio;
+
+            Raio = double.Parse(Console.ReadLine());
+
+            A =  Math.PI * (Raio * Raio);
+
+            Console.WriteLine("Area do círculo = " +A);
+            Console.ReadLine();
+
+        }
+
+    }
+}
