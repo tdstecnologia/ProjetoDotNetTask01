780f9c4b48145b4afda517c776eb1999d4352b59
Renato Rodrigues <luizrenatoporto@gmail.com>
2019-07-26 15:37:40 -0400

    Volume do Cilindro

---
+++ Renato/VolumeDoCilindro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDotNetTask01.Renato
+{
+    class VolumeDoCilindro
+    {
+        static void Main (string[]args)
+        {
+
+            double A, R, Altura;
+
+            R = double.Parse(Console.ReadLine());
+            Altura = double.Parse(Console.ReadLine());
+
+            A = Math.PI * (R * R) * Altura;
+            Console.WriteLine("Volume do Cilindor = " + A );
+            Console.ReadLine();
+
+        }
+
+    }
+}
