0c055c221f2c6f0061008808534b5213ea6d2891
Renato Rodrigues <luizrenatoporto@gmail.com>
2019-07-26 14:35:12 -0400

    Calcular Area do Quadrado

---
+++ Renato/CalcularAreaDoQuadrado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDotNetTask01.Renato
+{
+    class CalcularAreaDoQuadrado
+    {
+        static void Main (string[] args)
+        {
+            double Lado, A;
+
+            Lado = double.Parse(Console.ReadLine());
+
+            A = Lado * Lado;
+
+            Console.WriteLine( "Area do Quadrado"+A );
+            Console.ReadLine();
+
+        }
+
+    }
+}
