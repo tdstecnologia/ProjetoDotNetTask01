aef2810227618c8a4c75694d2fd6f871f1d80379
Renato Rodrigues <luizrenatoporto@gmail.com>
2019-07-26 13:54:32 -0400

    Renato

---
+++ Renato/CalcularAreaDoCirculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDotNetTask01.Renato
+{
+    class CalcularAreaDoCirculo
+    {
+        static void Main(string[] args)
+        {
+            double A, R;
+
+            R = double.Parse(Console.ReadLine());
+
+            A = Math.PI * (R * R);
+
+            Console.WriteLine(" Área do Raio = "+ A);
+            Console.ReadLine();
+        }
+    }
+}
